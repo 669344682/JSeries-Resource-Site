dc4243ad706eef8d6b424ab74773f75d34352477
CodyJ <codylewiz@gmail.com>
2018-06-13 01:43:12 +0100

    Test

--- server/fileserver.js
+++ server/fileserver.js
@@ -1,15 +1,9 @@
 var fs = Npm.require('fs');
-
-var path = Npm.require('path');
-Meteor.rootPath     = path.resolve('.');
-Meteor.absolutePath = Meteor.rootPath.split(path.sep + '.meteor')[0];
-var base = Meteor.absolutePath;
-
 WebApp.connectHandlers.use(function(req, res, next) {
     var re = /^\/files\/(.*)$/.exec(req.url);
     if (re !== null) {   // Only handle URLs that start with /files/ prefix
         // 
-        var filePath = base+"/Files/" + re[1];
+        var filePath = "C:/Users/Administrator/Desktop/Website/CommunityMTA/.static~/" + re[1];
         var data = fs.readFileSync(filePath);
 			if(!re[1].includes('resources')){
 				  res.writeHead(200, {
@@ -26,4 +20,4 @@ WebApp.connectHandlers.use(function(req, res, next) {
     } else {  // Other urls will have default behaviors
         next();
     }
-});
+}); 
